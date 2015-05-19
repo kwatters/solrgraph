d7a92e45e2c1aaec4dcd70e61c155ddf17d28a12
Dan Meehl <mr.cratos@gmail.com>
2015-05-19 16:14:43 -0400

    Add EqualsTerm class. Same as Term but implies that the field must exactly equal the value.

---
+++ src/main/java/com/kmwllc/search/solr/client/EqualsTerm.java
@@ -0,0 +1,54 @@
+package com.kmwllc.search.solr.client;
+
+import org.apache.tools.ant.taskdefs.condition.Equals;
+
+/**
+ * Created by dmeehl
+ * Created on 5/18/15
+ */
+public class EqualsTerm extends Term {
+
+  public EqualsTerm(String field, String term) {
+    this(field, term, -1);
+  }
+
+  public EqualsTerm(String field, String term, float boost) {
+    super(field, term, boost);
+    //EqualsTerm.SOLN = "EQUALS(";
+  }
+
+  //TODO:: Override toString with proper output
+  // currently Term just outputs "" + field + SEP + term + EOLN
+
+  @Override
+  public String toString() {
+    // we need to escape the term for the sep
+    String escaped = getTerm().replaceAll(SEP, "\\" + SEP);
+    if (escaped.contains(" ") || escaped.contains(",")) {
+      escaped = "\"" + escaped + "\"";
+    }
+    return "" + getField() + SEP + "EQUALS(" + escaped + "," + getBoost() + ")" + EOLN;
+  }
+
+  public static void main(String[] args){
+    EqualsTerm e = new EqualsTerm("aaa", "bbb", 1);
+    System.out.println(e);
+
+    Term t = new Term("aaa", "bbb", 1);
+    System.out.println(t);
+
+    RangeTerm r = new RangeTerm("aaa", "1234", "4567", 1);
+    System.out.println(r);
+    r = new RangeTerm("aaa", null, "4567", 1);
+    System.out.println(r);
+    r = new RangeTerm("aaa", "1234", null, 1);
+    System.out.println(r);
+
+    r = new RangeTerm("aaa", "1234", "4567", false, true);
+    System.out.println(r);
+    r = new RangeTerm("aaa", null, "4567", true, false);
+    System.out.println(r);
+    r = new RangeTerm("aaa", "1234", null, false, false);
+    System.out.println(r);
+  }
+}
